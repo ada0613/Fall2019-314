ba7273ca7bf5515a090388185cc25b7745c5c21b
ada0613 <49667167+ada0613@users.noreply.github.com>
2019-12-04 23:45:28 -0600

    Add files via upload

---
+++ Config.java
@@ -0,0 +1,8 @@
+package Phrase2;
+
+public class Config {
+	
+	public final static String DATAPATH = "data/"; // If you don't know what the static keyword does, you better go look it up now.
+	  // Prime file: One prime per line.
+	  // Cross file: Two primes per line, separated by a comma.
+	}
